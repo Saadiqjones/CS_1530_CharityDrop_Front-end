c06e051b2d5e286b030886de1be3a57ce09906f5
Saadiqjones <saj83@pitt.edu>
2021-03-30 16:15:44 -0400

    removed excess files, added components and assets

---
+++ src/Components/HeroPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HeroPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default HeroPage
